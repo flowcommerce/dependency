156a38b00071837ba00f38b4ec074791ae1c4281
Alex <alex@flow.io>
2018-02-07 09:40:02 -0500

    wip - projects spec split out test into two

--- api/test/controllers/ProjectsSpec.scala
+++ api/test/controllers/ProjectsSpec.scala
@@ -2,27 +2,26 @@ package controllers
 
 import java.util.UUID
 
-import play.api.test._
 import util.{DependencySpec, MockDependencyClient}
 
 class ProjectsSpec extends DependencySpec with MockDependencyClient {
 
   import scala.concurrent.ExecutionContext.Implicits.global
 
-  lazy val org = createOrganization()
-  lazy val project1 = createProject(org)()
-  lazy val project2 = createProject(org)()
-
   "GET /projects by id" in  {
-    await(
-      identifiedClient().projects.get(id = Some(project1.id))
-    ).map(_.id) must contain theSameElementsAs Seq(project1.id)
+    val org = createOrganization()
+    val project1 = createProject(org)
 
+    val x: Seq[io.flow.dependency.v0.models.Project] = await(identifiedClient().projects.get(id = Option(project1.id)))
+//    println("###" + x)
+    x.map(_.id) must contain theSameElementsAs Seq(project1.id)
+
+  }
+
+  "GET /projects by id that does not exist" in  {
     await(
-      identifiedClient().projects.get(id = Some(UUID.randomUUID.toString))
-    ).map(_.id) must be(
-      Nil
-    )
+      identifiedClient().projects.get(id = Option(UUID.randomUUID.toString))
+    ).map(_.id) must be(Nil)
   }
 
 
