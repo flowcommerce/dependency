9d9bed5324dde1ec93075eb746c3d9a4cd215aa5
Jakub Kozłowski <kubukoz@gmail.com>
2018-07-13 00:15:07 +0200

    Remove some imports

--- api/app/lib/DependencyProjectsModule.scala
+++ api/app/lib/DependencyProjectsModule.scala
@@ -1,8 +1,7 @@
 package lib
 
-import io.flow.dependency.api.lib.{DefaultGithub, Github, MockGithub}
 import io.flow.lib.dependency.DependencyProjects
-import play.api.{Configuration, Environment, Mode}
+import play.api.{Configuration, Environment}
 import play.api.inject.{Binding, Module}
 
 class DependencyProjectsModule extends Module {
