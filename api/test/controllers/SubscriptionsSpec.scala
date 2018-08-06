98d0c19c8790fe774d2c195d959f100fb053f8fb
Michael Bryzek <mike@flow.io>
2018-08-06 07:34:44 -0400

    auth

--- api/test/controllers/SubscriptionsSpec.scala
+++ api/test/controllers/SubscriptionsSpec.scala
@@ -10,26 +10,37 @@ class SubscriptionsSpec extends DependencySpec with MockDependencyClient {
 
   import scala.concurrent.ExecutionContext.Implicits.global
 
-  private[this] lazy val user1 = createUser()
-  private[this] lazy val user1Identifier = createUserIdentifier(user1)
-  private[this] val subscription1 = createSubscription(createSubscriptionForm(user1))
-  private[this] lazy val user2 = createUser()
-  private[this] lazy val user2Identifier = createUserIdentifier(user2)
-  private[this] val subscription2 = createSubscription(createSubscriptionForm(user2))
+  private[this] lazy val defaultUser1 = createUser()
+  private[this] lazy val defaultUser1Identifier = createUserIdentifier(defaultUser1)
+  private[this] val defaultSubscription1 = createSubscription(createSubscriptionForm(defaultUser1))
+  private[this] lazy val defaultUser2 = createUser()
+  private[this] lazy val defaultUser2Identifier = createUserIdentifier(defaultUser2)
+  private[this] val defaultSubscription2 = createSubscription(createSubscriptionForm(defaultUser2))
 
   "GET /subscriptions by identifier" in {
     await(
-      identifiedClient().subscriptions.get(identifier = Some(user1Identifier.value))
-    ).map(_.id) must contain theSameElementsAs Seq(subscription1.id)
+      identifiedClient().subscriptions.get(identifier = Some(defaultUser1Identifier.value))
+    ).map(_.id) must contain theSameElementsAs Seq(defaultSubscription1.id)
 
     await(
-      identifiedClient().subscriptions.get(identifier = Some(user2Identifier.value))
-    ).map(_.id) must contain theSameElementsAs Seq(subscription2.id)
+      identifiedClient().subscriptions.get(identifier = Some(defaultUser2Identifier.value))
+    ).map(_.id) must contain theSameElementsAs Seq(defaultSubscription2.id)
 
     await(
       identifiedClient().users.get(id = Some(UUID.randomUUID.toString))
     ).map(_.id) must be(
       Nil
     )
+  }
+
+  "DELETE /subscriptions/:id by identifier" in {
+    val user = createUser()
+    val user1Identifier = createUserIdentifier(user)
+    val subscription = createSubscription(createSubscriptionForm(user))
+
+    await(
+      identifiedClient().subscriptions.deleteById(id = subscription.id, identifier = Some(user1Identifier.value))
+    )
+    subscriptionsDao.findById(subscription.id).isEmpty must be(true)
   }
 }
