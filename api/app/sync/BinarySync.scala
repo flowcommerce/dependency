e661a85b5353be4223bb856271a40ad709dea72e
Ben Iofel <ben@flow.io>
2020-01-14 15:03:28 -0500

    log binary sync result

--- api/app/sync/BinarySync.scala
+++ api/app/sync/BinarySync.scala
@@ -5,6 +5,7 @@ import io.flow.common.v0.models.UserReference
 import io.flow.dependency.actors.SearchActor
 import io.flow.dependency.api.lib.DefaultBinaryVersionProvider
 import io.flow.dependency.v0.models.Binary
+import io.flow.log.RollbarLogger
 import io.flow.postgresql.Pager
 import javax.inject.Inject
 
@@ -14,12 +15,19 @@ class BinarySync @Inject() (
   binaryVersionsDao: BinaryVersionsDao,
   syncsDao: SyncsDao,
   @javax.inject.Named("search-actor") searchActor: akka.actor.ActorRef,
+  logger: RollbarLogger,
 ) {
 
   def sync(user: UserReference, binaryId: String): Unit = {
     binariesDao.findById(binaryId).foreach { binary =>
       syncsDao.withStartedAndCompleted("binary", binary.id) {
         defaultBinaryVersionProvider.versions(binary.name).foreach { version =>
+          logger
+            .fingerprint(s"${getClass.getName}:results")
+            .withKeyValue("binary", binary.name.toString)
+            .withKeyValue("version", version.toString)
+            .info("result")
+
           binaryVersionsDao.upsert(user, binary.id, version.value)
         }
       }
