b32480c58b6fc88a690c638b2a041cdab3c6c955
Juwen <juwen@flow.io>
2020-02-10 16:03:43 -0500

    update www project

--- www/app/controllers/ResolversController.scala
+++ www/app/controllers/ResolversController.scala
@@ -17,7 +17,7 @@ class ResolversController @javax.inject.Inject() (
   val config: Config,
   val controllerComponents: ControllerComponents,
   val flowControllerComponents: FlowControllerComponents
-)(implicit ec: ExecutionContext) extends BaseController(config, dependencyClientProvider) {
+)(implicit ec: ExecutionContext) extends controllers.BaseController(config, dependencyClientProvider) {
 
   override def section = Some(io.flow.dependency.www.lib.Section.Resolvers)
 
