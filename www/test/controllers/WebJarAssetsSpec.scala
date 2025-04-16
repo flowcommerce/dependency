7366df7af257ae7e2b162faad9e1ea239b9a6978
Jack Lynch <jack@flow.io>
2025-04-16 15:52:20 +0100

    spec

--- www/test/controllers/WebJarAssetsSpec.scala
+++ www/test/controllers/WebJarAssetsSpec.scala
@@ -1,19 +1,31 @@
 package controllers
 
+import org.scalatest.BeforeAndAfterAll
 import org.scalatestplus.play._
 import org.scalatestplus.play.guice.GuiceOneAppPerSuite
 import org.webjars.play.WebJarsUtil
 import play.api.test.Helpers._
 import play.api.test._
 
-class WebJarAssetsSpec extends PlaySpec with GuiceOneAppPerSuite with Injecting {
+class WebJarAssetsSpec extends PlaySpec with GuiceOneAppPerSuite with Injecting with BeforeAndAfterAll {
 
-  private val webJarsUtil = inject[WebJarsUtil]
+  private lazy val webJarsUtil = inject[WebJarsUtil]
+
+  // the first attempt to get a located resource always gives 404, so
+  // we compensate by running one before the tests.
+  override def beforeAll(): Unit = {
+    val url = webJarsUtil.locate("index.html").url.get
+    val result = route(app, FakeRequest(GET, url)).get
+    locally(status(result))
+    ()
+  }
 
   // Updates to webjars version can break serving of assets
   private def verifyCss(file: String) = {
-    val path = webJarsUtil.locate(file).url.get
-    val request = FakeRequest(GET, path)
+    locally(webJarsUtil.locate(file).url.get)
+    val url = webJarsUtil.locate(file).url.get
+
+    val request = FakeRequest(GET, url)
     val result = route(app, request).get
 
     status(result) mustBe OK
@@ -21,7 +33,7 @@ class WebJarAssetsSpec extends PlaySpec with GuiceOneAppPerSuite with Injecting 
   }
 
   "WebJar assets" should {
-    "bootstrap.min.css" ignore { // cannot work out why this fails - it resolves in the rendered page
+    "bootstrap.min.css" in {
       verifyCss("bootstrap.min.css")
     }
 
